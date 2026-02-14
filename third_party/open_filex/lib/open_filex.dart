09c6aca432d91d4cf06c90676cad51b115890d05
Phillip Assmann <p.assmann@nexus.gd>
2026-02-14 19:31:22 +0100

    move open filex to git

--- third_party/open_filex/lib/open_filex.dart
+++
@@ -1,8 +0,0 @@
-library open_filex;
-
-/// OpenResult is a class to inform result of platform
-export 'src/common/open_result.dart';
-
-/// Use platform specific codes or use web if it's the web
-export 'src/platform/open_filex.dart'
-    if (dart.library.html) 'src/web/open_filex.dart';
